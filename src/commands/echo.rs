8cb52c61361777b513dca1d2093623dfe7b868fc
Azz el arab <belazzelarab@gmail.com>
2025-09-07 02:39:58 +0100

    new line echo

--- src/commands/echo.rs
+++ src/commands/echo.rs
@@ -21,7 +21,8 @@ pub fn new_ligne(s : &str,a : bool) -> String {
            }else if count > 0 {
                 if count == 1 && !b {
                         temp = s.replace(&s[j..j+count+1], &s[j+count..j+count+1]);
-                } else if (count == 2  && a || (count == 3 && !b )  && a ) || count == 1 && b {
+                } else if ((count == 2 || count == 3 && !b ) && a) || count == 1 && b {
+                    println!("-----------");
                    let res =  match &s[j+count..j+count+1] {
                           "n" =>"\n",
                           "r" => "\r",
@@ -32,11 +33,11 @@ pub fn new_ligne(s : &str,a : bool) -> String {
                           "\\n" => "\n",
                           _ => &s[j+count-1..j+count+1]
                    };
-
                      temp = s.replace(&s[j..j+count+1], res);
-                 }else if a {
-                     temp = s.replace(&s[j..j+count+1], &s[j+count-1..j+count+1]);
-                 }else {
+                 }else if !a {
+                     let v =  count / 2;
+                     temp = s.replace(&s[j..j+count+1], &s[j+count-v..j+count+1]);
+                 }else{
                      temp = s.replace(&s[j..j+count+1], &s[j+count-1..j+count]);
                  }
                  j += count + 1;
