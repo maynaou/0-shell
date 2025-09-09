963566ea24148dd7e5227ad9136aa9e654130cfc
maynaou <mohssinaynaou874@gmail.com>
2025-09-09 21:34:03 +0100

    finale

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -2,16 +2,9 @@ pub mod echo;
 pub mod mkdir;
 pub mod ls;
 pub mod cat;
-<<<<<<< HEAD
-<<<<<<< HEAD
 pub mod cd;
 pub mod cp;
 pub mod pwd;
 pub mod mv;
 pub mod rm;
-=======
-pub mod cd;
->>>>>>> parent of 8fc6def (update cp vr finale)
-=======
-pub mod cd;
->>>>>>> parent of 8fc6def (update cp vr finale)
+
